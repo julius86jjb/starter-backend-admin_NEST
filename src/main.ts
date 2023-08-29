707b1b6e45d56b9ffad3150132c42280f62e271d
Julio Jimenez <julius.1986@gmail.com>
2023-08-29 12:41:24 +0100

    Config antes del despliegue

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     })
    );
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
